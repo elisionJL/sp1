0ffaf8e64f909ff494e04bd2c52edc8e83ad713b
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-16 10:47:07 +0800

    Create Entity.cpp

---
+++ Entity.cpp
@@ -0,0 +1,41 @@
+#include "Entity.h"
+
+int Entity::getHealth()
+{
+	return Health;
+}
+
+void Entity::setHealth(int health)
+{
+	this->Health = health;
+}
+
+int Entity::getDamage()
+{
+	return Damage;
+}
+
+void Entity::setDamage(int dmg)
+{
+	this->Damage = dmg;
+}
+
+int Entity::getResistance()
+{
+	return Resistance;
+}
+
+void Entity::setResistance(int resistance)
+{
+	this->Resistance = resistance;
+}
+
+int Entity::getSpeed()
+{
+	return Speed;
+}
+
+void Entity::setSpeed(int speed)
+{
+	this->Speed = speed;
+}
