b99d43cd0312fe8345709e6e167fa00f81293591
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-16 14:31:32 +0800

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,5 +1,3 @@
-#include "Entity.h"
-
 Entity::Entity()
 {
 }
@@ -46,65 +44,10 @@ void Entity::setSpeed(int speed)
 }
 string Entity::getMoveName(int MoveNo)
 {
-	switch (MoveNo)
-	{
-	case 1:
-		return Move1;
-	case 2:
-		return Move2;
-	case 3:
-		return Move3;
-	default:
-		return ;
-	}
-}
-void Entity::setMoveName(int MoveNo,string Name)
-{
-	if (MoveNo == 1)
-	{
-		Move1 = Name;
-	}
-	else if (MoveNo == 2)
-	{
-		Move2=Name;
-	}
-	else if (MoveNo == 3)
-	{
-		Move3=Name;
-	}
 }
 
 int Entity::getMovePower(int MoveNo)
 {
-	if (MoveNo==1)
-	{
-		return Move1Power;
-	}
-	else if (MoveNo == 2)
-	{
-		return Move2Power;
-	}
-	else if (MoveNo == 3)
-	{
-		return Move3Power;
-	}
-	else
-	{
-	return 0;
-	}
-}
-
-void Entity::setMovePower(int MoveNo, int Power)
-{
-	switch (MoveNo)
-	{
-	case 1:
-		Move1Power = Power;
-	case 2:
-		Move2Power = Power;
-	case 3:
-		Move3Power = Power;
-	}
 }
 
 Entity::~Entity()
