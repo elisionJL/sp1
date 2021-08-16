0512d4aee047470aacfb5d858b46234b8a9d980d
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-16 14:23:34 +0800

    Update Boss.cpp

--- Boss.cpp
+++ Boss.cpp
@@ -1,1 +1,50 @@
+#include "Boss.h"
+
+Boss::Boss()
+{
+	//Base boss stats
+	setHealth(200);
+	setDamage(25);
+	setResistance(50);
+	setSpeed(30);
+}
+
+string Boss::getMove4Name()
+{
+	return Move4;
+}
+
+void Boss::setMove4Name(string Name)
+{
+	Move4 = Name;
+}
+
+int Boss::getMove4Power()
+{
+	return Move4Power;
+}
+
+void Boss::setMove4Power(int Power)
+{
+	Move4Power = Power;
+}
+
+void Boss::ExecuteMove(int move)
+{
+	switch (move)
+	{
+	case 1:
+
+	case 2:
+
+	case 3:
+
+	case 4:
+
+	}
+}
+
+Boss::~Boss()
+{
+}
 
