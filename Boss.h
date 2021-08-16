212046ffa216449709575549fbc11d9c4f211cbf
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-16 14:22:37 +0800

    Update Boss.h

--- Boss.h
+++ Boss.h
@@ -1,5 +1,19 @@
-#
-
+#pragma once
+#include "Entity.h"
+using namespace std;
+class Boss : public Entity
+{
 private:
-
+	string Move4;
+	int Move4Power;
 public:
+	Boss();
+	string getMove4Name();
+	void setMove4Name(string Name);
+	int getMove4Power();
+	void setMove4Power(int Power);
+	void ExecuteMove(int move);
+	~Boss();
+
+	
+};
