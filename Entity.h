d0dfb4a3bd04fe066b8fcf5716c511936ba2c3bb
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-16 11:36:36 +0800

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 class Entity
 {
 private:
@@ -6,6 +7,10 @@ private:
 	int Damage;
 	int Resistance;
 	int Speed;
+	std::string Move1Name;
+	std::string Move2Name;
+	std::string Move3name;
+	std::string Move4name;
 
 public:
 	Entity();
@@ -17,5 +22,9 @@ public:
 	void setResistance(int resistance);
 	int getSpeed();
 	void setSpeed(int speed);
+	virtual void setMove1Name(std::string name);
+	virtual void setMove2Name(std::string name);
+	void setMove3Name(std::string name);
+	void setMove4Name(std::string name);
 	~Entity();
 };
