a0ea5c1315d88be92b341267e360fbffc6347973
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-16 14:40:56 +0800

    Update Boss.h

--- Boss.h
+++ Boss.h
@@ -4,16 +4,10 @@ using namespace std;
 class Boss : public Entity
 {
 private:
-	string Move4;
 	int Move4Power;
 public:
 	Boss();
-	string getMove4Name();
-	void setMove4Name(string Name);
 	int getMove4Power();
 	void setMove4Power(int Power);
-	void ExecuteMove(int move);
 	~Boss();
-
-	
 };
