e35b1584e1bee5806e3e8bd48dbe188b389e4969
ashafi04 <30901099+ashafi04@users.noreply.github.com>
2021-08-17 15:41:44 +0800

    Update Boss.cpp

--- Boss.cpp
+++ Boss.cpp
@@ -4,12 +4,21 @@ Boss::Boss()
 {
 	//Base boss stats
 	setHealth(200);
-	setDamage(25);
-	setResistance(50);
+	setDamage(50);
+	setResistance(20);
 	setSpeed(30);
+}
+
+string Boss::getMoveName(int MoveNo)
+{
+	return "BossGETNAME";
+}
+
+int Boss::getMovePower(int MoveNo)
+{
+	return 0;
 }
 
 Boss::~Boss()
 {
 }
-
