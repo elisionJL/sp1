543eaa685cacdba91ff421458f4bae46b910aca4
WappleLord <80618053+WappleLord@users.noreply.github.com>
2021-08-17 22:57:59 +0800

    Update player.h

--- player.h
+++ player.h
@@ -2,20 +2,22 @@
 #include<string>
 class player
 {
-	private:
-		int level;
-		int exp;
-		std::string name;
-		int charactersowned;
-		int coins;
-		std::string type;
-	public:
-		player(std::string a,std::string b);
-		~player();
-		void checklevelup();
-		void gacha(int x);
-		void stagecleared(int x, int y);
-		int getplayerinfo(int x);
-		std::string getplayerstrinfo(int x);
+private:
+	int level;
+	int exp;
+	std::string name;
+	int charactersowned;
+	int coins;
+	std::string type;
+	int highestclearedstage;
+public:
+	player(std::string a, std::string b);
+	~player();
+	void checklevelup();
+	void gacha(int x);
+	void stagecleared(int x, int y);
+	int getplayerinfo(int x);
+	std::string getplayerstrinfo(int x);
+	void clearednewstage();
+	int gethighestclearedstage();
 };
-
