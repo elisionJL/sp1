4a1348ad9af26fd3157c0bd52645ddf923360697
WappleLord <80618053+WappleLord@users.noreply.github.com>
2021-08-17 17:00:28 +0800

    Update player.h

--- player.h
+++ player.h
@@ -8,15 +8,13 @@ class player
 		std::string name;
 		int charactersowned;
 		int coins;
-		int characterlvlupmat;
 		std::string type;
 	public:
 		player(std::string a,std::string b);
 		~player();
 		void checklevelup();
 		void gacha(int x);
-		void stagecleared(int x, int y, int z);
-		void uselvlupmat(int x);
+		void stagecleared(int x, int y);
 		int getplayerinfo(int x);
 		std::string getplayerstrinfo(int x);
 };
